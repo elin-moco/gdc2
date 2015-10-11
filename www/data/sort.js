769c7559dd6d4c07d132d7034e770c281f4b3f12
Eddie Lin <elin@mozilla.com>
2015-10-11 22:54:14 +0800

    Added Top 10 coutries list.

--- www/data/sort.js
+++ www/data/sort.js
@@ -1,33 +1,34 @@
 function sortCountriesByContributionType(type, countries) {
-  countries.sort(function(a, b) {
-    if ('js' === type) {
-      a.score = a.languages.JavaScript;
-      a.score = a.score ? a.score : 0;
-      b.score = b.languages.JavaScript;
-      b.score = b.score ? b.score : 0;
-    } else if ('c' === type) {
-      a.score = (a.languages.C ? a.languages.C : 0) + (a.languages['C++'] ? a.languages['C++'] : 0);
-      b.score = (b.languages.C ? b.languages.C : 0) + (b.languages['C++'] ? b.languages['C++'] : 0);
-    } else if ('gaia' === type) {
-      a.score = (a.repositories['mozilla-b2g/gaia'] ? a.repositories['mozilla-b2g/gaia'] : 0);
-      b.score = (b.repositories['mozilla-b2g/gaia'] ? b.repositories['mozilla-b2g/gaia'] : 0);
-    } else {
-      a.score = Object.keys(a.languages).reduce(function(sum, key) {
-        return sum + parseInt(a.languages[key]);
-      }, 0);
-      b.score = Object.keys(b.languages).reduce(function(sum, key) {
-        return sum + parseInt(b.languages[key]);
-      }, 0);
+  return countries.sort(function(a, b) {
+    if (type) {
+      var isRepo = type.indexOf('/') > 0;
+      if (isRepo) {
+        a.contributions = a.repositories[type] || 0;
+        b.contributions = b.repositories[type] || 0;
+      } else if (type == 'sys') {
+        a.contributions = (a.languages.C || 0) + (a.languages['C++'] || 0);
+        b.contributions = (b.languages.C || 0) + (b.languages['C++'] || 0);
+      } else if (type == 'All') {
+        a.contributions = Object.keys(a.languages).reduce(function(sum, key) {
+          return sum + parseInt(a.languages[key]);
+        }, 0);
+        b.contributions = Object.keys(b.languages).reduce(function(sum, key) {
+          return sum + parseInt(b.languages[key]);
+        }, 0);
+      } else {
+        a.contributions = a.languages[type] || 0;
+        b.contributions = b.languages[type] || 0;
+      }
     }
-    return b.score - a.score;
+    return b.contributions - a.contributions;
   });
 }
 
-if (process) {
+if (typeof process !== 'undefined') {
   var countries = require('./events.json');
   var arg = process.argv[2];
   sortCountriesByContributionType(arg, countries)
   countries.forEach(function(country) {
-    console.log(country.name + ':\t' + country.score);
+    console.log(country.name + ':\t' + country.contributions);
   });
 }
